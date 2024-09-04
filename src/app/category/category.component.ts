469cafd53ced3c3c6df8bb1c1e95aca84f535593
FlorianB30 <96230889+FlorianB30@users.noreply.github.com>
2024-09-04 16:52:51 +0200

    manque le routage pour quizz

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -10,6 +10,7 @@ import { CategoryService } from '../shared/services/category.service';
 export class CategoryComponent implements OnInit {
   categories = this.categoriesService.categories; 
   categoryFilter: string = '';
+  categorySelected!: string;
 
   constructor(
     private router: Router,
@@ -17,7 +18,7 @@ export class CategoryComponent implements OnInit {
     private categoriesService: CategoryService
   ) {
     this.categoriesService.getAllCategories(this.categoryFilter)
-   }
+  }
 
   ngOnInit(): void {
     // ICI ALIMENTER this.categories = l'obserbver du service (recup le name des categ en fct de l input de la barre de recherche)
@@ -25,5 +26,9 @@ export class CategoryComponent implements OnInit {
       // this.categoryService.;
     });
     console.log(this.categories)
+  }
+
+  setCategorySelected(selected: any): void {
+    this.categorySelected = selected
   }
 }
