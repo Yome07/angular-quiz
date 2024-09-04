8ce197b4e58521cb0142528fc68cbc3f5539fb24
amirtalbi <amirotalbi94@gmail.com>
2024-09-04 17:07:22 +0200

    feat: adding searchbar

--- src/app/shared/services/category.service.ts
+++ src/app/shared/services/category.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  categories!:any
+  categories!: any;
 
   constructor(private http: HttpClient) { }
 
-  getAllCategories(categoryFilter: string): void {
-    this.http
-      .get(`http://localhost:3000/categories`)
-      .subscribe((categories: any) => {
-        this.categories = categories;
-        console.log(this.categories)
-      });
+  getAllCategories(categoryFilter: string): Observable<any> {
+    return this.http.get(`http://localhost:3000/categories`);
   }
 }
